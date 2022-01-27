4961885aefc8782b04a7ef8b76d923c39ceaa775
BotTheWarrior <71945772+BotTheWarrior@users.noreply.github.com>
2022-01-27 18:31:34 +0100

    Update FARM.lua

--- FARM.lua
+++ FARM.lua
@@ -3,12 +3,12 @@ local team = game:GetService("ReplicatedStorage").Events.JoinTeam
 local data = game:GetService("Workspace").Status
 local v_u  = game:GetService('VirtualUser')
 
-local script_version = '1.1'
+local script_version = '1.2'
 local script_creator = 'SomeoneIdfk'
 local github = 'https://raw.githubusercontent.com/BotTheWarrior/scripts/main/FARM.lua'
 
 print('[FARM] Version:', script_version)
-print('[FARM] Info: This was made with scripts found online and heavily modified to customize it to my liking.')
+print('[FARM] Info: This was made with scripts found online and heavily modified to customize it to my liking. It also uses the Hexagon hub.')
 print('[FARM] Warning: This is still in beta and is not garanteed to always work.')
 print('[FARM] Creator:', script_creator)
 print('[FARM] Github:', github)
@@ -16,80 +16,103 @@ print('[FARM] Github:', github)
 print('[FARM] Loading...')
 
 worker = 0
-value = 0
+fvalue = 0
+svalue = 0
 last_team = 'Spectator'
+last_team_try = 'Spectator'
+team:FireServer('Spectator')
 
 --Auto-team to join a team automatically.
 local function win_check()
 
     worker = worker + 1
 
-    --If the last round is tied and if either team is below 5 wins, server-hop.
-    if data.TWins.Value == 7 and data.CTWins.Value == 7 then
-        Serverhop()
-    elseif data.TWins.Value < 5 and data.CTWins.Value < 5 then
-        Serverhop()
 
-    elseif data.TWins.Value ~= data.CTWins.Value then
-        --Check if T wins are above 4 and try and join it.
-        --Check if wins are above 4.
-        if data.TWins.Value > 4 and data.TWins.Value > data.CTWins.Value then
-            --Check if wins are higher then the competition.
-            --Loop so you don't get kicked for switching teams to fast.
-            if value ~= 30 then
-                value = value + 1
-            else
-                value = 0
 
-                if game:GetService('Players').LocalPlayer.Team ~= last_team then
-                    print('[FARM][T] Trying to join the team. >', game:GetService('Players').LocalPlayer.Team, '<')
-                    team:FireServer('T')
-                    if game:GetService('Players').LocalPlayer.Team ~= 'Counter-Terrorists' then
-                        last_team = game:GetService('Players').LocalPlayer.Team
-                        print('[FARM][T] Joined the team. >', last_team, '<')
+    if fvalue ~= 60 then
+        fvalue = fvalue + 1
+
+        --If the first or last round is tied, server-hop.
+        if data.TWins.Value == 7 and data.CTWins.Value == 7 then
+            Serverhop()
+        elseif data.TWins.Value == 0 and data.CTWins.Value == 0 then
+            Serverhop()
+
+        elseif data.TWins.Value ~= data.CTWins.Value then
+            --Check if T wins are above 4 and try and join it.
+            --Check if wins are above 4.
+            if svalue == 60 then
+                svalue = 0
+                if data.TWins.Value > 4 and data.TWins.Value > data.CTWins.Value then
+                    if tostring(game:GetService('Players').LocalPlayer.Team) ~= last_team then
+                        if last_team_try ~= 'Terrorists' then
+                            last_team_try = 'Terrorists'
+                            print('[FARM][T] Trying to join the team. >', game:GetService('Players').LocalPlayer.Team, '<')
+                        end
+                        
+                        team:FireServer('T')
+                        if tostring(game:GetService('Players').LocalPlayer.Team) == 'Terrorists' then
+                            last_team = tostring(game:GetService('Players').LocalPlayer.Team)
+                            print('[FARM][T] Joined the team. >', last_team, '<')
+                        --else
+                            --if last_team == 'Spectator' then
+                                --team:FireServer('CT')
+                            --end
+                        end
                     end
-                else
-                    team:FireServer('T')
+            
+
+                --Check if CT wins are above 4 and try and join it.
+                --Check if wins are above 4.
+                elseif data.CTWins.Value > 4 and data.CTWins.Value > data.TWins.Value then
+                    if tostring(game:GetService('Players').LocalPlayer.Team) ~= last_team then
+                        if last_team_try ~= 'Counter-Terrorists' then
+                            last_team_try = 'Counter-Terrorists'
+                            print('[FARM][CT] Trying to join the team. >', game:GetService('Players').LocalPlayer.Team, '<')
+                        end
+                        
+                        team:FireServer('CT')
+                        if tostring(game:GetService('Players').LocalPlayer.Team) == 'Counter-Terrorists' then
+                            last_team = tostring(game:GetService('Players').LocalPlayer.Team)
+                            print('[FARM][CT] Joined the team. >', last_team, '<')
+                        --else
+                            --if last_team == 'Spectator' then
+                                --team:FireServer('T')
+                            --end
+                        end
+                    end
                 end
-            end
-
-        --Check if CT wins are above 4 and try and join it.
-        --Check if wins are above 4.
-        elseif data.CTWins.Value > 4 and data.CTWins.Value > data.TWins.Value then
-            --Check if wins are higher then the competition.
-            --Loop so you don't get kicked for switching teams to fast.
-            if value ~= 30 then
-                value = value + 1
             else
-                value = 0
+                svalue = svalue + 1
+            end
+        end
+    else
+        fvalue = 0
 
-                if game:GetService('Players').LocalPlayer.Team ~= last_team then
-                    print('[FARM][CT] Trying to join the team. >', game:GetService('Players').LocalPlayer.Team, '<')
-                    team:FireServer('CT')
-                    if game:GetService('Players').LocalPlayer.Team ~= 'Terrorists' then
-                        last_team = game:GetService('Players').LocalPlayer.Team
-                        print('[FARM][CT] Joined the team. >', last_team, '<')
-                    end
-                else
-                    team:FireServer('CT')
+        if plr.Character.Humanoid.Health ~= 0 then
+            if data.TWins.Value >= data.CTWins.Value and tostring(game:GetService('Players').LocalPlayer.Team) == 'Terrorists' then
+                --Anti-afk so you won't get kicked for being afk.
+                wait(6)
+                if plr.Character.Humanoid.Health ~= 0 then
+                    print('[FARM][Anti-AFK] Setting health to 0 so you will hopefully not be kicked for being idle. (Not very well tested and will improve this system in the future.)')
+
+                    plr.Character.Humanoid.Health = 0
+                end
+
+            elseif data.CTWins.Value >= data.TWins.Value and tostring(game:GetService('Players').LocalPlayer.Team) == 'Counter-Terrorists' then
+                --Anti-afk so you won't get kicked for being afk.
+                wait(6)
+                if plr.Character.Humanoid.Health ~= 0 then
+                    print('[FARM][Anti-AFK] Setting health to 0 so you will hopefully not be kicked for being idle. (Not very well tested and will improve this system in the future.)')
+
+                    plr.Character.Humanoid.Health = 0
                 end
             end
         end
     end
 end
 
-
-
 print('[FARM] Loaded auto-team.')
-
-
---Anti-afk so you won't get kicked for being afk.
-game:GetService("Players").LocalPlayer.Idled:connect(function()
-    v_u:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-    wait(1)
-    v_u:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
- end)
-
 print('[FARM] Loaded anti-afk.')
 
 
@@ -110,7 +133,14 @@ end
 
 print('[FARM] Loaded server-hop.')
 
+if data.TWins.Value < 5 and data.CTWins.Value < 5 then
+    Serverhop()
+    wait(10)
+end
 
+repeat wait() until game:IsLoaded()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ionlol/Hexagon-fixed-for-July-28-Roblox-update-/main/fix"))();
 
 --Loop the auto-team.
 game:GetService('RunService'):BindToRenderStep('win_check',190,win_check)

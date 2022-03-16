0f1d5c32494e3251a825eae93673fcb2dffc8647
SomeoneIdfk <71945772+SomeoneIdfk@users.noreply.github.com>
2022-03-16 20:30:44 +0100

    update

--- HexagonModifiedAlpha.lua
+++ HexagonModifiedAlpha.lua
@@ -18,7 +18,7 @@ local RunService = game:GetService("RunService")
 local getrawmetatable = getrawmetatable or false
 local mousemove = mousemove or mousemoverel or mouse_move or false
 local getsenv = getsenv or false
-local senv = getsenv(game.Players.LocalPlayer.PlayerGui.Client)senv.splatterBlood = function() end
+--local senv = getsenv(game.Players.LocalPlayer.PlayerGui.Client)senv.splatterBlood = function() end
 local listfiles = listfiles or listdir or syn_io_listdir or false
 local isfolder = isfolder or false
 local hookfunc = hookfunc or hookfunction or replaceclosure or false

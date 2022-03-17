3bf6de38615473149c65ca91e296b77cf4cb933c
SomeoneIdfk <71945772+SomeoneIdfk@users.noreply.github.com>
2022-03-17 01:50:09 +0100

    update

--- library.lua
+++ library.lua
@@ -158,7 +158,7 @@ local library = {
         guiname = "Hexagon",
 		title = "Hexagon [SomeoneIdfk Edition]",
 		logo = "rbxassetid://4350178803", -- "http://www.roblox.com/asset/?id=6597976562",
-        footer = "",
+        footer = "SomeoneIdfk Modified it -_-",
         modal = true,
         font = Enum.Font.SourceSans,
         textsize = 16,

f794b57f1241febfcc564356be1130aff4fe6380
SomeoneIdfk <71945772+SomeoneIdfk@users.noreply.github.com>
2022-03-25 11:59:48 +0100

    update

--- HexagonModifiedAlpha.lua
+++ HexagonModifiedAlpha.lua
@@ -1597,51 +1597,56 @@ local ExperimentalTabCategoryOptions = ExperimentalTab:AddCategory("Options", 1)
 ExperimentalTabCategoryOptions:AddToggle("Texture Remover", false ,"ExperimentalTabCategoryOptionsTR", function(val)
 	if val == true then
 		currentmap = nil
-		TextureRemoverLoop = game:GetService("RunService").RenderStepped:Connect(function()
+		TextureRemoverLoop = game:GetService("RunService").Stepped:Connect(function()
 			pcall(function()
-				if currentmap ~= WorkSpace.Map.Origin.Value then
-					Hint.Text = "Removing textures..."
-					local decalsyeeted = true
-					local g = game
-					local w = g.Workspace
-					local l = g.Lighting
-					local t = w.Terrain
-					t.WaterWaveSize = 0
-					t.WaterWaveSpeed = 0
-					t.WaterReflectance = 0
-					t.WaterTransparency = 0
-					l.GlobalShadows = false
-					l.FogEnd = 9e9
-					l.Brightness = 0
-					for i, v in pairs(g:GetDescendants()) do
-					    if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
-					        v.Material = "Plastic"
-						    v.Reflectance = 0
-					    elseif v:IsA("Decal") or v:IsA("Texture") and decalsyeeted then
-					        v.Transparency = 1
-					    elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
-					        v.Lifetime = NumberRange.new(0)
-					    elseif v:IsA("Explosion") then
-					        v.BlastPressure = 1
- 					        v.BlastRadius = 1
-					    elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
-					        v.Enabled = false
-					    elseif v:IsA("MeshPart") then
-					        v.Material = "Plastic"
-					        v.Reflectance = 0
-					        v.TextureID = 30117799
-					    end
+				if trueorfalse10 == true then
+					if currentmap ~= WorkSpace.Map.Origin.Value then
+						Hint.Text = "Removing textures..."
+						local decalsyeeted = true
+						local g = game
+						local w = g.Workspace
+						local l = g.Lighting
+						local t = w.Terrain
+						t.WaterWaveSize = 0
+						t.WaterWaveSpeed = 0
+						t.WaterReflectance = 0
+						t.WaterTransparency = 0
+						l.GlobalShadows = false
+						l.FogEnd = 9e9
+						l.Brightness = 0
+						for i, v in pairs(g:GetDescendants()) do
+							if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
+								v.Material = "Plastic"
+								v.Reflectance = 0
+							elseif v:IsA("Decal") or v:IsA("Texture") and decalsyeeted then
+								v.Transparency = 1
+							elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+								v.Lifetime = NumberRange.new(0)
+							elseif v:IsA("Explosion") then
+								v.BlastPressure = 1
+								 v.BlastRadius = 1
+							elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
+								v.Enabled = false
+							elseif v:IsA("MeshPart") then
+								v.Material = "Plastic"
+								v.Reflectance = 0
+								v.TextureID = 30117799
+							end
+						end
+						for i, e in pairs(l:GetChildren()) do
+							if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
+								e.Enabled = false
+							end
+						end
+	
+						currentmap = WorkSpace.Map.Origin.Value
+						Hint.Destroy()
+					else
+						wait(1)
 					end
-					for i, e in pairs(l:GetChildren()) do
-    					if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
-        					e.Enabled = false
-    					end
-					end
-
-					currentmap = WorkSpace.Map.Origin.Value
-					Hint.Destroy()
 				else
-					wait(1)
+					wait(5)
+					trueorfalse10 = true
 				end
 			end)
 		end)

87c73b316847e095e09ce55d4b07e2fc82c658c9
SomeoneIdfk <71945772+SomeoneIdfk@users.noreply.github.com>
2022-03-26 16:09:22 +0100

    update

--- HexagonLoader.lua
+++ HexagonLoader.lua
@@ -19,26 +19,20 @@ if isfile("hexagon/load_version.txt") then
 end
 
 if not isfile("hexagon/load_version.txt") then
-	local VersionWindow = library:CreateWindow(Vector2.new(350, 100), Vector2.new((workspace.CurrentCamera.ViewportSize.X/2)-250, (workspace.CurrentCamera.ViewportSize.Y/2)-250))
+	local VersionWindow = library:CreateWindow(Vector2.new(300, 200), Vector2.new((workspace.CurrentCamera.ViewportSize.X/2)-250, (workspace.CurrentCamera.ViewportSize.Y/2)-250))
 
 	local VersionTab = VersionWindow:CreateTab("Version")
 	local VersionTabOptions = VersionTab:AddCategory("Options", 1)
 	VersionTabOptions:AddDropdown("Branch", {"Hexagon Modified", "Skin Changer"}, "Hexagon Modified", "Branch")
 	VersionTabOptions:AddButton("Save", function()
-		print(1)
 		if library.pointers.Branch.value == "Hexagon Modified" then
-			print(2)
 			if library.pointers.Build.value == "Stable" then
-				print(3)
 				writefile("hexagon/load_version.txt", "https://raw.githubusercontent.com/SomeoneIdfk/scripts/main/HexagonModified.lua")
 			elseif library.pointers.Build.value == "Alpha" then
-				print(4)
 				writefile("hexagon/load_version.txt", "https://raw.githubusercontent.com/SomeoneIdfk/scripts/main/HexagonModifiedAlpha.lua")
 			end
 		elseif library.pointers.Branch.value == "Skin Changer" then
-			print(5)
 			if library.pointers.Build.value == "-" then
-				print(6)
 				writefile("hexagon/load_version.txt", "https://raw.githubusercontent.com/SomeoneIdfk/scripts/main/HexagonSkinChanger.lua")
 			end
 		end

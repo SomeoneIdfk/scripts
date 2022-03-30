8bd165b87d7939894e1ad927dd17cca441ba323c
SomeoneIdfk <71945772+SomeoneIdfk@users.noreply.github.com>
2022-03-30 06:59:39 +0200

    update

--- hexagon/HexagonModifiedAlpha.lua
+++ hexagon/HexagonModifiedAlpha.lua
@@ -1617,6 +1617,7 @@ local ExperimentalTab = Window:CreateTab("Experimental")
 
 local ExperimentalTabCategoryOptions = ExperimentalTab:AddCategory("Options", 1)
 
+ExperimentalTabCategoryOptions:AddDropdown("Texture Remove Method", {"Legacy", "New"}, "New", "ExperimentalTabCategoryOptionsTRM")
 ExperimentalTabCategoryOptions:AddToggle("Texture Remover", false ,"ExperimentalTabCategoryOptionsTR", function(val)
 	if val == true then
 		currentmap = nil
@@ -1627,77 +1628,118 @@ ExperimentalTabCategoryOptions:AddToggle("Texture Remover", false ,"Experimental
 						currentmap = WorkSpace.Map.Origin.Value
 						wait(3)
 						Hint.Text = "Removing textures..."
-						local decalsyeeted = true -- Leaving this on makes games look shitty but the fps goes up by at least 20.
-						local g = game
-						local w = g.Workspace
-						local l = g.Lighting
-						local t = w.Terrain
-						sethiddenproperty(l,"Technology",2)
-						sethiddenproperty(t,"Decoration",false)
-						t.WaterWaveSize = 0
-						t.WaterWaveSpeed = 0
-						t.WaterReflectance = 0
-						t.WaterTransparency = 0
-						l.GlobalShadows = 0
-						l.FogEnd = 9e9
-						l.Brightness = 0
-						for i, v in pairs(w:GetDescendants()) do
-							if v:IsA("BasePart") and not v:IsA("MeshPart") then
-								v.Material = "Plastic"
-								v.Reflectance = 0
-							elseif (v:IsA("Decal") or v:IsA("Texture")) and decalsyeeted then
-								v.Transparency = 1
-							elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
-								v.Lifetime = NumberRange.new(0)
-							elseif v:IsA("Explosion") then
-								v.BlastPressure = 1
-								v.BlastRadius = 1
-							elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
-								v.Enabled = false
-							elseif v:IsA("MeshPart") and decalsyeeted then
-								v.Material = "Plastic"
-								v.Reflectance = 0
-								v.TextureID = 10385902758728957
-							elseif v:IsA("SpecialMesh") and decalsyeeted  then
-								v.TextureId=0
-							elseif v:IsA("ShirtGraphic") and decalsyeeted then
-								v.Graphic=0
-							elseif (v:IsA("Shirt") or v:IsA("Pants")) and decalsyeeted then
-								v[v.ClassName.."Template"]=0
+
+						if library.pointers.ExperimentalTabCategoryOptionsTRM.value == "Legacy" then
+							local decalsyeeted = true
+							local g = game
+							local w = g.Workspace
+							local l = g.Lighting
+							local t = w.Terrain
+							t.WaterWaveSize = 0
+							t.WaterWaveSpeed = 0
+							t.WaterReflectance = 0
+							t.WaterTransparency = 0
+							l.GlobalShadows = false
+							l.FogEnd = 9e9
+							l.Brightness = 0
+							for i, v in pairs(g:GetDescendants()) do
+								if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
+									v.Material = "Plastic"
+									v.Reflectance = 0
+								elseif v:IsA("Decal") or v:IsA("Texture") and decalsyeeted then
+									v.Transparency = 1
+								elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+									v.Lifetime = NumberRange.new(0)
+								elseif v:IsA("Explosion") then
+									v.BlastPressure = 1
+									v.BlastRadius = 1
+								elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
+									v.Enabled = false
+								elseif v:IsA("MeshPart") then
+									v.Material = "Plastic"
+									v.Reflectance = 0
+									v.TextureID = 1521636846
+								end
 							end
+							for i, e in pairs(l:GetChildren()) do
+								if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
+									e.Enabled = false
+								end
+							end
+						elseif library.pointers.ExperimentalTabCategoryOptionsTRM.value == "New" then
+							local decalsyeeted = true -- Leaving this on makes games look shitty but the fps goes up by at least 20.
+							local g = game
+							local w = g.Workspace
+							local l = g.Lighting
+							local t = w.Terrain
+							sethiddenproperty(l,"Technology",2)
+							sethiddenproperty(t,"Decoration",false)
+							t.WaterWaveSize = 0
+							t.WaterWaveSpeed = 0
+							t.WaterReflectance = 0
+							t.WaterTransparency = 0
+							l.GlobalShadows = 0
+							l.FogEnd = 9e9
+							l.Brightness = 0
+							for i, v in pairs(w:GetDescendants()) do
+								if v:IsA("BasePart") and not v:IsA("MeshPart") then
+									v.Material = "Plastic"
+									v.Reflectance = 0
+								elseif (v:IsA("Decal") or v:IsA("Texture")) and decalsyeeted then
+									v.Transparency = 1
+								elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+									v.Lifetime = NumberRange.new(0)
+								elseif v:IsA("Explosion") then
+									v.BlastPressure = 1
+									v.BlastRadius = 1
+								elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
+									v.Enabled = false
+								elseif v:IsA("MeshPart") and decalsyeeted then
+									v.Material = "Plastic"
+									v.Reflectance = 0
+									v.TextureID = 10385902758728957
+								elseif v:IsA("SpecialMesh") and decalsyeeted  then
+									v.TextureId=0
+								elseif v:IsA("ShirtGraphic") and decalsyeeted then
+									v.Graphic=0
+								elseif (v:IsA("Shirt") or v:IsA("Pants")) and decalsyeeted then
+									v[v.ClassName.."Template"]=0
+								end
+							end
+							for i = 1,#l:GetChildren() do
+								e=l:GetChildren()[i]
+								if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
+									e.Enabled = false
+								end
+							end
+							w.DescendantAdded:Connect(function(v)
+								wait()--prevent errors and shit
+								if v:IsA("BasePart") and not v:IsA("MeshPart") then
+									v.Material = "Plastic"
+									v.Reflectance = 0
+								elseif v:IsA("Decal") or v:IsA("Texture") and decalsyeeted then
+									v.Transparency = 1
+								elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+									v.Lifetime = NumberRange.new(0)
+								elseif v:IsA("Explosion") then
+									v.BlastPressure = 1
+									v.BlastRadius = 1
+								elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
+									v.Enabled = false
+								elseif v:IsA("MeshPart") and decalsyeeted then
+									v.Material = "Plastic"
+									v.Reflectance = 0
+									v.TextureID = 10385902758728957
+								elseif v:IsA("SpecialMesh") and decalsyeeted then
+									v.TextureId=0
+								elseif v:IsA("ShirtGraphic") and decalsyeeted then
+									v.ShirtGraphic=0
+								elseif (v:IsA("Shirt") or v:IsA("Pants")) and decalsyeeted then
+									v[v.ClassName.."Template"]=0
+								end
+							end)
 						end
-						for i = 1,#l:GetChildren() do
-							e=l:GetChildren()[i]
-							if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
-								e.Enabled = false
-							end
-						end
-						w.DescendantAdded:Connect(function(v)
-							wait()--prevent errors and shit
-						   if v:IsA("BasePart") and not v:IsA("MeshPart") then
-								v.Material = "Plastic"
-								v.Reflectance = 0
-							elseif v:IsA("Decal") or v:IsA("Texture") and decalsyeeted then
-								v.Transparency = 1
-							elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
-								v.Lifetime = NumberRange.new(0)
-							elseif v:IsA("Explosion") then
-								v.BlastPressure = 1
-								v.BlastRadius = 1
-							elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
-								v.Enabled = false
-							elseif v:IsA("MeshPart") and decalsyeeted then
-								v.Material = "Plastic"
-								v.Reflectance = 0
-								v.TextureID = 10385902758728957
-							elseif v:IsA("SpecialMesh") and decalsyeeted then
-								v.TextureId=0
-							elseif v:IsA("ShirtGraphic") and decalsyeeted then
-								v.ShirtGraphic=0
-							elseif (v:IsA("Shirt") or v:IsA("Pants")) and decalsyeeted then
-								v[v.ClassName.."Template"]=0
-							end
-						end)
+						
 						Hint.Destroy()
 					else
 						wait(1)

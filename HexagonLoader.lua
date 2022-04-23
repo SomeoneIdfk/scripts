de577e38be7f32dd2b26ac524f9fd6fd208c88b9
SomeoneIdfk <71945772+SomeoneIdfk@users.noreply.github.com>
2022-04-23 15:29:52 +0200

    update

--- HexagonLoader.lua
+++ HexagonLoader.lua
@@ -7,8 +7,13 @@ repeat wait() until game.Players.LocalPlayer.PlayerGui:FindFirstChild("GUI")
 
 local listfiles = listfiles or listdir or syn_io_listdir or false
 local isfolder = isfolder or false
+local isfile = isfile or false
 
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/SomeoneIdfk/scripts/main/hexagon/lib/library_loader.lua"))()
+
+writefile("hexagon/versions.cfg", game:HttpGet("https://raw.githubusercontent.com/SomeoneIdfk/scripts/main/versions.cfg"))
+
+local versions = loadstring("return "..readfile("hexagon/versions.cfg"))()
 
 if not isfolder("hexagon") then
 	makefolder("hexagon")
@@ -27,58 +32,57 @@ local function checkId()
 	return false
 end
 
-if isfile("hexagon/load_version.txt") then
-	if checkId() then
-		if readfile("hexagon/load_version.txt") == "https://raw.githubusercontent.com/SomeoneIdfk/scripts/main/hexagon/HexagonModified.lua" then
-			loadstring(game:HttpGet(readfile("hexagon/load_version.txt")))();
-		elseif readfile("hexagon/load_version.txt") == "https://raw.githubusercontent.com/SomeoneIdfk/scripts/main/hexagon/HexagonModifiedAlpha.lua" then
-			loadstring(game:HttpGet(readfile("hexagon/load_version.txt")))();
-		elseif readfile("hexagon/load_version.txt") == "https://raw.githubusercontent.com/SomeoneIdfk/scripts/main/hexagon/HexagonSkinChanger.lua" then
-			loadstring(game:HttpGet(readfile("hexagon/load_version.txt")))();
-		end
-	elseif not checkId() then
-		game.Players.LocalPlayer:Kick("Hexagon Modified is not for this game.")
-	end
+local function checkFile()
+    if isfile("hexagon/load_version.txt") then
+        local temp = readfile("hexagon/load_version.txt")
+        local result = table.foreach(versions["data"], function(i, v)
+            local result = table.foreach(v["data"], function(i2, v2)
+                if v2 == temp then
+                    return true
+                end
+            end)
+            if result then
+                return true
+            end
+        end)
+    
+        if result == true then
+            return true
+        elseif result == false then
+            return false
+        end
+    elseif not isfile("hexagon/load_version.txt") then
+        return false
+    end
 end
 
-if not isfile("hexagon/load_version.txt") then
-	if checkId() then
+if checkId() then
+	if checkFile() then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/SomeoneIdfk/scripts/main/"..readfile("hexagon/load_version.txt")))();
+	elseif not checkFile() then
 		local VersionWindow = library:CreateWindow(Vector2.new(350, 200), Vector2.new((workspace.CurrentCamera.ViewportSize.X/2)-250, (workspace.CurrentCamera.ViewportSize.Y/2)-250))
-
+	
 		local VersionTab = VersionWindow:CreateTab("Version")
 		local VersionTabOptions = VersionTab:AddCategory("Options", 1)
-		VersionTabOptions:AddDropdown("Branch", {"Hexagon Modified", "Skin Changer"}, "Hexagon Modified", "Branch")
+		VersionTabOptions:AddDropdown("Branch", {"-"}, "-", "Branch")
 		VersionTabOptions:AddButton("Save", function()
-			if library.pointers.Branch.value == "Hexagon Modified" then
-				if library.pointers.Build.value == "Stable" then
-					writefile("hexagon/load_version.txt", "https://raw.githubusercontent.com/SomeoneIdfk/scripts/main/hexagon/HexagonModified.lua")
-				elseif library.pointers.Build.value == "Alpha" then
-					writefile("hexagon/load_version.txt", "https://raw.githubusercontent.com/SomeoneIdfk/scripts/main/hexagon/HexagonModifiedAlpha.lua")
-				end
-			elseif library.pointers.Branch.value == "Skin Changer" then
-				if library.pointers.Build.value == "-" then
-					writefile("hexagon/load_version.txt", "https://raw.githubusercontent.com/SomeoneIdfk/scripts/main/hexagon/HexagonSkinChanger.lua")
-				end
-			end
+            writefile("hexagon/load_version.txt", versions["data"][library.pointers.Branch.value]["data"][library.pointers.Build.value])
 			game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
 		end)
 		local VersionTabOptions2 = VersionTab:AddCategory("", 2)
 		VersionTabOptions2:AddDropdown("Build", {"-"}, "-", "Build")
 
-		game:GetService("RunService").Stepped:Connect(function()
-			pcall(function()
-				if library.pointers.Branch.value == "Hexagon Modified" then
-					library.pointers.Build.options = {"Stable", "Alpha"}
-					if library.pointers.Build.value == "-" then
-						library.pointers.Build:Set("Stable")
-					end
-				elseif library.pointers.Branch.value == "Skin Changer" then
-					library.pointers.Build.options = {"-"}
-					if library.pointers.Build.value ~= "-" then
-						library.pointers.Build:Set("-")
-					end
-				end
-			end)
-		end)
+        library.pointers.Branch.options = versions["tables"]
+        library.pointers.Branch:Set(versions["tables"][1])
+
+		while true do
+            library.pointers.Build.options = versions["data"][library.pointers.Branch.value]["tables"]
+            if not table.find(versions["data"][library.pointers.Branch.value]["tables"], library.pointers.Build.value) then
+                library.pointers.Build:Set(versions["data"][library.pointers.Branch.value]["tables"][1])
+            end
+            wait()
+        end
 	end
+elseif not checkId() then
+	game.Players.LocalPlayer:Kick("Running on the wrong game!")
 end
